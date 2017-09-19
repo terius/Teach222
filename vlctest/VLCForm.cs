9917cb3d9e8646b1ed50bda1e4c931ce7c329103
teriu <teriu@DESKTOP-11706O7>
2017-09-19 22:51:13 +0800

    aaaaaaa

---
+++ vlctest/VLCForm.cs
@@ -0,0 +1,29 @@
+﻿using AxAXVLC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vlctest
+{
+    public partial class VLCForm : Form
+    {
+        private AxVLCPlugin2 axVLCPlugin21;
+        public VLCForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VLCForm_Load(object sender, EventArgs e)
+        {
+            axVLCPlugin21 = new AxVLCPlugin2();
+            axVLCPlugin21.Dock = DockStyle.Fill;
+            this.Controls.Add(axVLCPlugin21);
+        }
+    }
+}
